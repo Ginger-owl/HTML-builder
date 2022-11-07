0dabcdff0f32d997a9efec1bc7dca613087c5e6f
Ginger-owl <r.s.uteshev@gmail.com>
2022-11-08 02:19:13 +0300

    feat: add solution to task 4 copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,34 @@
+const path = require('path');
+const fs = require('fs');
 
+const folderPath = path.resolve(__dirname, 'files');
+const folderCopyPath = path.resolve(__dirname, 'files-copy');
+
+fs.stat(folderCopyPath, err => {
+  if (!err) {
+    console.log('Directory with copies exists, updating contents...')
+  } else if (err.code === 'ENOENT') {
+    console.log('Directory with copies does not exist - copying...')
+    fs.mkdir(folderCopyPath, { recursive:true}, err => {
+      if (err) {
+        console.log("Error", err);
+      }
+    })
+  }
+})
+
+fs.readdir(folderPath,
+  (err, files) => {
+  if (err) {
+    console.log("Error: ", err)
+  } else {
+    files.forEach(file => {
+      fs.copyFile(path.resolve(folderPath, file), path.join(folderCopyPath, file), (err) => {
+        if (err) {
+          console.log("Error: ", err);
+        }
+      })
+    })
+  }
+  console.log("Finished!\nCopied files are up-to-date!")
+});
