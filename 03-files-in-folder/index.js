0fc280f1344524e7f6b7afef8bdc7add07e3f637
Ginger-owl <r.s.uteshev@gmail.com>
2022-11-08 02:18:47 +0300

    feat: add solution for task 3 files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const fs = require('fs')
+
+const folderPath = path.resolve(__dirname, 'secret-folder')
+
+fs.readdir(folderPath,
+  (err, files) => {
+  if (err) {
+    console.log("Error: ", err)
+  } else {
+    files.forEach(file => {
+        fs.stat(path.resolve(folderPath, file), (err, stats) => {
+          if (err) {
+            console.log("Error - ", err)
+          } else {
+            if (stats.isFile()) {
+              const fileName = file.split('.')[0];
+              const fileType = path.extname(file).replaceAll('.','');
+              const fileSize = stats.size;
+              console.log(`${fileName} - ${fileType} - ${fileSize}kb`);
+            }
+          }
+        });
+      })
+    }
+  });
