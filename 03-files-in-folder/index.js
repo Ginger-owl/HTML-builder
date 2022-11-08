29bada67f4118e674f14123df0776efc30f67db3
Ginger-owl <r.s.uteshev@gmail.com>
2022-11-08 18:20:32 +0300

    feat: added round to 3 decimal places

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -16,7 +16,7 @@ fs.readdir(folderPath,
             if (stats.isFile()) {
               const fileName = file.split('.')[0];
               const fileType = path.extname(file).replaceAll('.','');
-              const fileSize = stats.size / 1024;
+              const fileSize = Math.round((stats.size / 1024) * 1000)  / 1000;
               console.log(`${fileName} - ${fileType} - ${fileSize}kb`);
             }
           }
