a58c84f674ff586615a709d25dcbd7308e6aae91
Ginger-owl <r.s.uteshev@gmail.com>
2022-11-11 15:42:29 +0300

    refactor/feat: turn to single async func, add recursive assets update

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,139 +1,71 @@
-/* Импорт всех требуемых модулей
-Прочтение и сохранение в переменной файла-шаблона
-Нахождение всех имён тегов в файле шаблона
-Замена шаблонных тегов содержимым файлов-компонентов
-Запись изменённого шаблона в файл index.html в папке project-dist
-Использовать скрипт написанный в задании 05-merge-styles для создания файла style.css
-Использовать скрипт из задания 04-copy-directory для переноса папки assets в папку project-dist */
-
 const fs = require('fs');
 const path = require('path');
 
-// path to resulting dir
-const resultPath = path.resolve(__dirname, 'project-dist');
 
-// path to styles
-const stylesPartsPath = path.resolve(__dirname, 'styles');
+try {
+  (async function() {
+    // create result dir 
+    await fs.promises.rm(path.resolve(__dirname, 'project-dist'), {recursive: true, force: true})
+    await fs.promises.mkdir(path.resolve(__dirname,'project-dist'), {recursive: true})
 
-//path to assets copy
-const assetsPath = path.resolve(__dirname, 'assets');
-const assetsCopyPath = path.resolve(resultPath, 'assets');
-
-// stream to write styles and index files
-const outputStyles = fs.createWriteStream(path.resolve(resultPath, 'style.css'));
-const outputTemplate = fs.createWriteStream(path.resolve(resultPath, 'index.html'));
-
-// Template
-const templatePath = path.resolve(__dirname, 'template.html');
-const componentsPath = path.resolve(__dirname, 'components');
-
-// general pattern for templates
-const generalPattern = /{{\w+}}/i;
-
-
-// create project-dist
-fs.mkdir(resultPath, { recursive:true }, err => {
-  if (err) {
-    console.log("Error", err);
-  }
-  //gather styles
-  fs.readdir(stylesPartsPath,
-    (err, files) => {
-    if (err) {
-      console.log("Error: ", err);
-    } else {
-      files.forEach(file => {
-        const fileType = path.extname(file);
-          fs.stat(path.resolve(stylesPartsPath, file), (err, stats) => {
-            if (err) {
-              console.log("Error - ", err);
-            } else {
-              /* Проверка является ли объект файлом и имеет ли файл нужное расширение */
-              if (stats.isFile() && fileType === '.css') {
-                /* Чтение файла стилей */
-                fs.readFile(path.resolve(stylesPartsPath, file), (err, data) => {
-                  if (err) {
-                    console.log("Error - ", err);
-                    /* 5. Запись прочитанных стилей данных в в файл  */
-                  } else {
-                    outputStyles.write(data + "\n");
-                  }
-                })
-              }
-            }
-          });
+    // gather styles to style.css
+    const StyleFiles = await fs.promises.readdir(path.resolve(__dirname, 'styles'), {withFileTypes: true})
+    const stylesStream = fs.createWriteStream(path.resolve(__dirname, 'project-dist', 'style.css'))
+    StyleFiles.forEach(async (style) => {
+      // path.extname(style) === '.css'
+      if(style.name.split('.')[1] === 'css') {
+        fs.createReadStream(path.resolve(__dirname, 'styles', style.name), 'utf8').addListener('data', data => {
+          stylesStream.write(data);
         })
       }
-      console.log("+ Styles gathered together!")
-  });
-  
-  // copy assests
-  fs.mkdir(assetsCopyPath, { recursive:true }, err => {
-    if (err) {
-      console.log("Error", err);
-    }
-    fs.readdir(assetsPath,
-      (err, dirs) => {
-      if (err) {
-        console.log("Error: ", err)
-      } else {
-        dirs.forEach(dir => {
-            fs.mkdir(path.resolve(assetsCopyPath, dir), { recursive:true }, err => {
-              if (err) {
-                console.log("Error", err);
-              }
-            });
-            fs.readdir(path.resolve(assetsPath, dir), (err, files) => {
-              if (err) {
-                console.log("Error: ", err)
-              } else {
-                files.forEach(file => {
-                  fs.copyFile(path.resolve(assetsPath, dir, file), path.resolve(assetsCopyPath, dir , file), (err) => {
-                    if (err) {
-                      console.log("Error: ", err);
-                    }
-                  })
-                })
-              }
-            })
-          })
+    })
+
+    // create html, from template n components 
+    await fs.promises.copyFile(path.resolve(__dirname, 'template.html'), path.resolve(__dirname, 'project-dist', 'index.html'))
+
+    let htmlData = await fs.promises.readFile(path.resolve(__dirname, 'project-dist', 'index.html'), 'utf8')
+
+    const components = await fs.promises.readdir(path.resolve(__dirname, 'components'), {withFileTypes: true})
+    components.forEach(async (file) => {
+      const htmlFile = await fs.promises.readFile(path.resolve(__dirname, 'components', `${file.name}`))
+
+      htmlData = htmlData.replace(`{{${file.name.split('.')[0]}}}`, htmlFile)
+
+      await fs.promises.writeFile(path.resolve(__dirname, 'project-dist', 'index.html'), htmlData, err => {
+        if (err) {
+          console.log(err)
         }
       })
-      console.log("+ Assests collected and up-to-date!")
-    });
-  })
-
-
-
-
-// read
-fs.readFile(templatePath, 'utf-8', (err, data) => {
-if (err) {
-  console.log("Error: ", err)
-}
-//let dataStr = data;
-fs.readdir(componentsPath,
-  (err, files) => {
-    if (err) {
-      console.log("Error: ", err)
-    }
-    files.forEach(file => {
-      const name = file.split('.')[0];
-      const pattern = new RegExp(`{{${name}}}`)
-      //console.log(pattern);
-      //console.log(pattern.test(data))
-      if (pattern.test(data)) {
-        //console.log('in')
-        fs.readFile(path.resolve(componentsPath, file), (err, html) => {
-          data = data.replace(pattern, html.toString());
-          if (!generalPattern.test(data)) {
-            outputTemplate.write(data);
-            console.log("+ Page is ready!")
-
-          }
-        });
-      }
     })
-  })
-});
-  
+
+    // copy assets to result dir
+    // copy dir
+    const copyFolder = async (folderPath, CopyPath) => {
+      await fs.promises.rm(CopyPath, { recursive: true, force: true }, err => {
+        if (err) {
+          console.log("Error", err);
+        }
+      })
+    
+      await fs.promises.mkdir(CopyPath, { recursive: true }, err => {
+        if (err) {
+          console.log("Error", err);
+        }
+      })
+      
+      fs.promises.readdir(folderPath, { withFileTypes: true})
+      .then(files => files.forEach(file => {
+        if (file.isDirectory()) {
+          copyFolder(path.resolve(folderPath, file.name), path.resolve(CopyPath, file.name))
+        } 
+        if (file.isFile()) {
+          fs.promises.copyFile(path.resolve(folderPath, file.name), path.resolve(CopyPath, file.name))
+        }
+      }))
+    }
+
+    copyFolder(path.resolve(__dirname, 'assets'), path.resolve(__dirname, 'project-dist', 'assets'))
+  })()
+} catch (err) {
+  console.log(err)
+}
