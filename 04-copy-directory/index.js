10fdc07ea8c1617e4b1a972cec725f81e9fb7122
Ginger-owl <r.s.uteshev@gmail.com>
2022-11-11 14:39:01 +0300

    feat: add check for changes in files, turn to async function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,34 +1,36 @@
 const path = require('path');
 const fs = require('fs');
+const fsPromises = require('fs/promises')
 
-const folderPath = path.resolve(__dirname, 'files');
-const folderCopyPath = path.resolve(__dirname, 'files-copy');
+let folderPath = path.resolve(__dirname, 'files');
+let copyFolderPath = path.resolve(__dirname, 'files-copy');
 
-fs.stat(folderCopyPath, err => {
-  if (!err) {
-    console.log('Directory with copies exists, updating contents...')
-  } else if (err.code === 'ENOENT') {
-    console.log('Directory with copies does not exist - copying...')
-    fs.mkdir(folderCopyPath, { recursive:true}, err => {
+let copyFolder = async (folderPath, CopyPath) => {
+  await fsPromises.rm(CopyPath, { recursive: true, force: true }, err => {
+    if (err) {
+      console.log("Error", err);
+    }
+  })
+
+  await fsPromises.mkdir(CopyPath, { recursive: true }, err => {
+    if (err) {
+      console.log("Error", err);
+    }
+  })
+
+  fs.readdir(folderPath, (err, files) => {
       if (err) {
-        console.log("Error", err);
+        console.log("Error: ", err)
+      } else {
+        files.forEach(file => {
+          fs.copyFile(path.resolve(folderPath, file), path.resolve(CopyPath, file), err => {
+            if (err) {
+              console.log("Error", err);
+            }
+          });
+        })
       }
-    })
-  }
-})
+    });
+}
 
-fs.readdir(folderPath,
-  (err, files) => {
-  if (err) {
-    console.log("Error: ", err)
-  } else {
-    files.forEach(file => {
-      fs.copyFile(path.resolve(folderPath, file), path.join(folderCopyPath, file), (err) => {
-        if (err) {
-          console.log("Error: ", err);
-        }
-      })
-    })
-  }
-  console.log("Finished!\nCopied files are up-to-date!")
-});
+copyFolder(folderPath, copyFolderPath)
